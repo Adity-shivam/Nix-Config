c179d2a36dc41fe5e05dbae7e6e51585a4deb898
Adity-Shivam <adishivam1507@gmail.com>
2026-04-21 13:38:50 +0100

    fixing home manager

--- flake.nix
+++ flake.nix
@@ -2,24 +2,24 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs"; # inputs.home-manager.inputs.nixpkgs_unstable is same as inputs.nixpkgs_unstable
-     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+     inputs.nixpkgs.follows = "nixpkgs-unstable"; # inputs.home-manager.inputs.nixpkgs is same as inputs.nixpkgs_unstable and do not reinstall two version of nixpkgs/nixos-unstable
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs-unstable, ... }@inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
 
     # Use sudo nixos-rebuild switch --flake .#Nix-Chan to use this
-    nixosConfigurations.Nix-Chan = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;}; # inherit input is same as inputs = inputs
+    nixosConfigurations.Nix-Chan = nixpkgs-unstable.lib.nixosSystem {
+      specialArgs = {inherit inputs;}; # inherit input is same as inputs = inputs. This let us use inputs in the modules below
       modules = [
         ./hosts/HP-Victus/configuration.nix
-         inputs.home-manager.nixosModules.default 
+        inputs.home-manager.nixosModules.Nix-Chan
       ];
     };
   };
